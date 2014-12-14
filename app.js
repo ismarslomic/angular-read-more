3e6458de25dec6cb4adf9b0f095a189947bc039d
Hitesh Modha <hiteshmodha10@gmail.com>
2014-12-14 17:59:34 +0530

    reached at 50%

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var app = angular.module("app", []);
 
-app.controller('readmore',function($scope){
+app.controller('readmoreCntroller',function($scope){
 	$scope.text = "hello there hello therehello therehello therehello therehello therehello therehello there hello therehello therehello therehello there ";
 });
+
+app.directive('hmRead', function () {
+    return {
+    	restrict:'AE',
+    	scope:{
+    		hmtext : '@',
+    		hmlimit : '@',
+    		hminit:'@'
+    	},
+        templateUrl: 'template.html',
+        link : function(scope,elem,attr){
+        		
+        	
+        
+        }
+    };
+});
+
