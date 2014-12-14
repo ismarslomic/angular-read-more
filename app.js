d1a95eaecdc6511cc033698365bda1e1220322d5
Hitesh Modha <hiteshmodha10@gmail.com>
2014-12-14 13:22:18 +0530

    changes in controller and view

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 var app = angular.module("app", []);
+
+app.controller('readmore',function($scope){
+	$scope.text = "hello there hello therehello therehello therehello therehello therehello therehello there hello therehello therehello therehello there ";
+});
