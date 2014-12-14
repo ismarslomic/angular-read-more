e306d2b5dbe78ecf5ad88faee6e1ae5ed7672c62
Hitesh Modha <hiteshmodha10@gmail.com>
2014-12-14 23:51:26 +0530

    completed basic functionality

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var app = angular.module("app", []);
 
 app.controller('readmoreCntroller',function($scope){
-	$scope.text = "hello there hello therehello therehello therehello therehello therehello therehello there hello therehello therehello therehello there ";
+	$scope.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non vulputate nisl. Curabitur placerat tincidunt nisl eu gravida. Phasellus vitae consequat neque, sed scelerisque ipsum. Nunc dictum consequat sem, tincidunt rhoncus orci hendrerit nec. Fusce vel dictum felis. Nam non risus nisl. Etiam fermentum ut ligula sed porta. Aenean consequat feugiat nulla at vulputate. Maecenas non risus nisl. Pellentesque dolor ex, venenatis at justo a, porttitor elementum mi. Etiam rutrum ac urna vitae egestas. Morbi efficitur ipsum nisi.";
+    //$scope.text="hj";
 });
 
 app.directive('hmRead', function () {
@@ -10,13 +11,23 @@ app.directive('hmRead', function () {
     	scope:{
     		hmtext : '@',
     		hmlimit : '@',
-    		hminit:'@'
+    		hmfulltext:'@',
+            hmMoreText:'@',
+            hmLessText:'@',
+            hmMoreClass:'@',
+            hmLessClass:'@'
     	},
         templateUrl: 'template.html',
-        link : function(scope,elem,attr){
-        		
-        	
-        
+        controller : function($scope){
+        	  $scope.toggleValue=function(){
+
+                    if($scope.hmfulltext == true)
+                        $scope.hmfulltext=false;
+                    else if($scope.hmfulltext == false)
+                        $scope.hmfulltext=true;
+                    else
+                        $scope.hmfulltext=true;
+              }        
         }
     };
 });
