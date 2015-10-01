02ea88ffb2b025d4f343e5a6d19324e03e6ec8d8
Ismar Slomic <ismar@slomic.no>
2015-10-02 00:00:00 +0200

    added ./ in gulfile for coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('karma', ['build'], function (done) {
 gulp.task('coveralls', function () { // 2nd arg is a dependency: 'karma' must be finished first.
 	// Send results of istanbul's test coverage to coveralls.io.
 	return gulp.src('gulpfile.js', {read: false}) // You have to give it a file, but you don't have to read it.
-		.pipe(shell('cat coverage/report-lcov/lcov.info | node_modules/coveralls/bin/coveralls.js'));
+		.pipe(shell('cat ./coverage/report-lcov/lcov.info | ./node_modules/coveralls/bin/coveralls.js'));
 });
 
 gulp.task('test', ['karma', 'coveralls']);
